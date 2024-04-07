ec643b855ded5484180439651b16cf5087d534e5
abascharles <abascharlesbenard@gmail.com>
2024-04-07 10:08:27 +0300

    Listening to keyboard event - using esc key to close modal window

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -7,7 +7,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden'); // Ensure the overlay is also shown when the modal is opened
 };
@@ -27,11 +26,25 @@ const closeModal = function () {
 //remove()
 //toggle(Remove if present, add if not) - used intead of add an remove.
 //replace(old class, new class) - replaces old class with new class     ('enabled', 'disabled')
-//contains() like bolean if element contain a class it returns true if not it returns false
+//contains()  if element contain a class it returns true if not it returns false
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
 }
 
+//clossing the modal
+
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//handling keypress events - keyboard events are global events (they
+//                           cannot happen on one element only) hence
+//                           they are listed on the whole document.
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
